e61bb84415aa0cf6e3e224e92f4f740418a6ee86
cesar1.5 <cesar_jcmc@hotmail.com>
2014-11-12 00:02:41 -0500

    todas las vistas create y algo de servicetest

---
+++ TransportesEDVI/Edvi.Web/Services/VehiculoServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Data.Entity;
+using NUnit.Framework;
+using Moq;
+using Edvi.Services.Service;
+using Edvi.DB.DB;
+
+namespace Edvi.Web.Services
+{
+    [TestFixture]
+    public class VehiculoServiceTest
+    {
+    }
+}
